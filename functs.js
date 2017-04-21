690a11c6505341d957aa22edbec4bceb9f5871d9
clintfidel <clintfidel@icloud.comgit config --global user.email clintfidel@icloud.comgit config --global user.email clintfidel@icloud.com>
2017-04-21 18:57:45 +0100

    i modify my files

--- functs.js
+++ functs.js
@@ -1,63 +1,84 @@
 'use strict';
+let vorpal = require('vorpal')();
 var config = require('./config'); //importing the config file
 let firebase = require('firebase'); //importing the firebase module
-let prompt = require('prompt'); //Importing the prompt module
-
+var Jusibe = require('jusibe');
+var jusibe = new Jusibe("fa2b6a5fe2019548f05b34f17819d5a1", "8c5e94d8c3fe93c4da9d5c087ac32a9a");
 firebase.initializeApp(config);
 var database = firebase.database(); //creating an instance of firebase
 var ref = database.ref("/users");
 
-
 var getDataFromDataBase = {
-  addToDataBase: function(firstName,lastName, phoneNumber){
-    database.ref('/users').push ({        
-    firstName: firstName,
-    lastName: lastName,
-    phoneNumber: phoneNumber,
-  })
-  return true;
+
+  addToDataBase: function(firstName, lastName, phoneNumber) {
+    database.ref('/users').push({
+      firstName: firstName,
+      lastName: lastName,
+      phoneNumber: phoneNumber,
+    })
+    return true;
   },
 
-  fetchFromDataBase: function(){
+  fetchFromDataBase: function() {
     ref.on("value", function(snapshot) {
-  var valx = snapshot.val();
-  console.log(Object.values(valx));
-  }, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
+        var valx = snapshot.val();
+        console.log(Object.values(valx));
+      },
+      function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+      });
+
   },
- search: function(searchTerm){
+
+  search: function(searchTerm) {
     ref.on("value", function(snapshot) {
       var valx = snapshot.val();
       var data = Object.values(valx);
+      var bool = false;
       var myArr = [];
-  for(var i = 0; i < data.length; i++){
-      if(searchTerm === data[i].firstName){
-        myArr.push([data[i],data[i].lastName, 1]);
+      for (var i = 0; i < data.length; i++) {
+        if (searchTerm === data[i].firstName) {
+          myArr.push([data[i], data[i].lastName, 1]);
+        } else if (searchTerm === data[i].lastName) {
+          myArr.push([data[i], data[i].firstName, 2]);
+        }
       }
-      else if(searchTerm === data[i].lastName){
-        myArr.push([data[i], data[i].firstName, 2]);
+      if (myArr.length === 0) {
+        console.log("The name can not be found");
+      } else if (myArr.length === 1) {
+        console.log("The number is: " + myArr[0][0].phoneNumber);
+      } else {
+        var message = "Which " + searchTerm + "?";
+        for (var j = 0; j < myArr.length; j++) {
+          message = message + "  " + "[" + (j + 1).toString() + "]" + " " + myArr[j][1];
+          bool = true;
+        }
       }
-    }
-    if(myArr.length === 0){
-      console.log("The name can not be found");
-    }
-    else if(myArr.length === 1){
-      console.log("The number is: " + myArr[0][0].phoneNumber);
-    }
-    else{
-      var mess = "Which " + searchTerm + "?";
-      for(var j = 0; j < myArr.length; j++){
-        mess = mess + "  " + "[" + (j+1).toString() + "]" + " " + myArr[j][1];
-      }
-    }
-    console.log(mess);
-}, function (errorObject) {
-  console.log("The read failed: " + errorObject.code);
-});
+      if (bool) {
+        console.log(message);
 
-  
-}
+      }
+    });
+  },
+
+  //  sendSms: function(num, msg){
+  //        var payload = {
+  //          to: 07038399928,
+  //          from: 'Clintfidel',
+  //          message: hello world
+  //        };
+
+  //       jusibe.sendSMS(payload, function (err, res) {
+  //        if (res.statusCode === 200)
+  //          console.log(res.body);
+  //        else
+  //          console.log(err);
+  //      }); 
+  // }
+
+  // function (errorObject) {
+  // console.log("The read failed: " + errorObject.code);
+
 }
 
 module.exports = getDataFromDataBase;
--- myApp.js
+++ myApp.js
@@ -1,55 +1,70 @@
 'use strict';
 
 let vorpal = require('vorpal')();
-let fireBase = require('./functs.js');
- vorpal
-   .command('add -n <name> <phoneNumber>', 'Add user datas')
-   .option('-n, --name', "Adds full name e.g 'Andela Clinton' ")
-   .option('-p, --phoneNumber', "Adds phone number e.g 080******** ")
-   .description('Outputs "save".')
-   .action(function(args, callback) {
+console.log("WELCOME TO MY CONTACT MANAGER!!")
+console.log("An app that allows you add a contact to your database, search for an existing contact,,sends a text to a contact.\n")
+console.log("Guide:\t")
+console.log("Follow this guide and you are on your way to exploring this great app.")
+console.log(' add -n "name" -p "phoneNumber " : "adds a contact " ', 'search "name": searches for a contact name and returns the number\n, text (name) -m: sends a message to a contact')
+
+let fireBase = require('./functs.js')
+vorpal
+  .command('add <name> <phoneNumber>', 'Add user datas')
+  .option('-n, --name', "Adds full name e.g 'Andela Clinton' ")
+  .option('-p, --phoneNumber', "Adds phone number e.g 080******** ")
+  .description('Outputs "save".')
+  .action(function(args, callback) {
     let new_number = args.phoneNumber; //Convert user input to a string so we can get the length
     let fullName = args.name;
     new_number = "0" + new_number.toString();
-   	let contactName = fullName.split(" "); //Splitting the names value to get first and last name
-   	//if(fullName.length > 3 && new_number.length===11){ //Validate the length of user's input
-   	if(fireBase.addToDataBase(contactName[0], contactName[1], new_number)){
-      fireBase.fetchFromDataBase();
-   		console.log("Added Successfully!")
-   	}
-      
-     callback();
-   });
+    let contactName = fullName.split(" "); //Splitting the names value to get first and last name
+    //if(fullName.length > 3 && new_number.length===11){ //Validate the length of user's input
+    if (fireBase.addToDataBase(contactName[0], contactName[1], new_number)) {
 
-   vorpal
-   .command('search <name>', 'searches for a users data')
-   .option('search', "searchs for users firstname e.g 'Andela' ")
-   .description('Outputs "search"')
-   .action(function(args, callback) {
-    var searchTerms = args.name;
-    fireBase.search(searchTerms);
-    
+      console.log("Added Successfully!")
+    }
 
     callback();
-  });     
+  });
 
+vorpal
+  .command('search <name>', 'searches for a users data')
+  .option('search', "searchs for users firstname e.g 'Andela' ")
+  .description('Outputs "search"')
+  .action(function(args, callback) {
+    var searchTerms = args.name;
+    fireBase.search(searchTerms);
+    // var arrayOfSameName = fireBase.search(searchTerms)
+    callback();
+  });
 
+// vorpal
+//    .command('text <name>', 'Sends sms to contact')
+//    .option('-m,--text <message>', "Sends specified message to user")
+//    .description('Outputs "search"')
+//    .action(function(args, callback) {
+//     var new_num= args.name;
+//     var new_message= args.text
+//      fireBase.sendSms(07038399928, new_message);
+//      // if(fireBase.sendSms===search)
+//      console.log("text successfully sent")
+//     callback();
+//   });     
 
+vorpal
+  .delimiter('Contact-App$')
+  .show();
 
- vorpal
-   .delimiter('Contact-App$')
-   .show();
+// function checkName(name){
+//  if(name.match(/[w_]/+)){
+//    return true;
+//  }
+//  return false;
+// }
 
-  // function checkName(name){
-  // 	if(name.match(/[w_]/+)){
-  // 		return true;
-  // 	}
-  // 	return false;
-  // }
-
-  // function checkNum(num){
-  // 	if(num.match(/[\d]/)){
-  // 		return true;
-  // 	}
-  // 	return false;
-  // }
+// function checkNum(num){
+//  if(num.match(/[\d]/)){
+//    return true;
+//  }
+//  return false;
+// }
